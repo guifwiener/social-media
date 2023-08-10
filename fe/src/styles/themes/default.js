41fb91430861d86b7fc787951553c63f2f59c077
guifwiener <gfw1994@gmail.com>
2023-08-10 07:41:16 -0300

    Add App, Routes and Global styles

---
+++ fe/src/styles/themes/default.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      main: '#65e4cf',
+      dark: '#23c2a7',
+      light: '#b7f5ea',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+      100: '#E6E6E6',
+    },
+    danger: {
+      main: '#dc3c3c',
+      dark: '#991a1a',
+      light: '#ee9292',
+    },
+  },
+};
